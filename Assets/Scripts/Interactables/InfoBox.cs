279bd6ac9de4d55f6f32a91e4d7849ea2af1b200
Adam Podoxin <adam.podoxin@gmail.com>
2021-08-15 12:13:18 -0700

    Add info box mechanic

---
+++ Assets/Scripts/Interactables/InfoBox.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InfoBox : Interactable
+{
+    [TextArea(5, 10)]
+    public string infoText;
+
+    [Space]
+
+    public Transform infoBoxObject;
+    public Text infoBoxText;
+
+    private bool isReading = false;
+
+    private Transform camTransform;
+
+    private Vector3 _lookPos;
+    private Vector3 _rotation;
+
+    private void Start()
+    {
+        infoBoxText.text = infoText;
+        camTransform = Camera.main.transform;
+
+        UpdateBox();
+    }
+
+    private void Update()
+    {
+        if (isReading)
+        {
+            _lookPos = camTransform.position - infoBoxObject.position;
+            _lookPos.y = 0;
+
+            _rotation = Quaternion.LookRotation(_lookPos).eulerAngles;
+            infoBoxObject.eulerAngles = _rotation + Vector3.up * 180f;
+        }
+    }
+
+    private void UpdateBox()
+    {
+        infoBoxObject.gameObject.SetActive(isReading);
+        interactText = isReading ? "Close" : "Read";
+    }
+
+    public override void Interact()
+    {
+        isReading = !isReading;
+        UpdateBox();
+    }
+}
