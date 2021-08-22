170e7d2b2a1abbd46da6810da76e0e000ac32834
Adam Podoxin <adam.podoxin@gmail.com>
2021-08-22 12:41:48 -0700

    Add screenshot capability

---
+++ Assets/Scripts/Cinematics/CameraScreenshot.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class CameraScreenshot : MonoBehaviour
+{
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha0))
+        {
+            TakeScreenshot();
+        }
+    }
+
+    public void TakeScreenshot()
+    {
+        string path = Application.persistentDataPath + "/Screenshots/" + DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss") + ".png";
+        print(path);
+
+        ScreenCapture.CaptureScreenshot(path);
+        UnityEditor.AssetDatabase.Refresh();
+    }
+}
