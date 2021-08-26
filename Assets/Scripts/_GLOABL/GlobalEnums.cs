6acb7749dac8b824c50d6ec5b27a4011be45ec32
Adam Podoxin <adam.podoxin@gmail.com>
2021-08-26 12:10:09 -0700

    Create choice cinematic

--- Assets/Scripts/_GLOABL/GlobalEnums.cs
+++ Assets/Scripts/_GLOABL/GlobalEnums.cs
@@ -2,7 +2,7 @@ public class GlobalEnums
 {
     public enum DemonState { Chasing, Travelling, Searching, Despawning, Killing };
 
-    public enum GroundType { Null, Wood, Carpet, Tile, Concrete };
+    public enum GroundType { Null, Wood, Carpet, Tile, Concrete, Demon };
 
     public enum ArtifactType { Null, Car, Bear, Flowers };
 }
