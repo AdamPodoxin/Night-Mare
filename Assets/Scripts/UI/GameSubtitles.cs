c24f06374b0d00242e8eb3e09d3fbeb9b92d7692
Adam Podoxin <adam.podoxin@gmail.com>
2021-08-30 16:57:37 -0700

    Fix subtitles duration issues

--- Assets/Scripts/UI/GameSubtitles.cs
+++ Assets/Scripts/UI/GameSubtitles.cs
@@ -14,30 +14,18 @@ public class GameSubtitles : MonoBehaviour
         anim = GetComponent<Animator>();
     }
 
-    private IEnumerator ShowSubtitlesCoroutine(string text, float duration)
-    {
-        subtitlesText.text = text;
-        anim.Play("Subtitles_FadeIn");
-
-        yield return new WaitForSeconds(duration + 0.25f);
-
-        if (anim.GetCurrentAnimatorStateInfo(0).IsName("Subtitles_On"))
-            anim.Play("Subtitles_FadeOut");
-    }
-
     public void ShowSubtitles(string text)
     {
         if (!gameObject.activeInHierarchy) return;
-
-        StopCoroutine(ShowSubtitlesCoroutine(text, 3f));
-        StartCoroutine(ShowSubtitlesCoroutine(text, 3f));
+        ShowSubtitles(text, 3f);
     }
 
     public void ShowSubtitles(string text, float duration)
     {
         if (!gameObject.activeInHierarchy) return;
 
-        StopCoroutine(ShowSubtitlesCoroutine(text, duration));
-        StartCoroutine(ShowSubtitlesCoroutine(text, duration));
+        subtitlesText.text = text;
+        anim.Play("Subtitles_FadeIn");
+        anim.SetFloat("subtitlesSpeed", 1f / duration);
     }
 }
